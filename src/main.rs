560e477dbc3c79b9f2b8efeb525654e37cecada1
Ghostik-gh <feodor200@mail.ru>
2022-07-26 00:30:54 +0300

    construct robot | fn for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::f32::consts::PI;
-
 use bevy::{
     core::FixedTimestep,
+    math::Vec3Swizzles,
     // input::keyboard::KeyboardInput,
     prelude::*,
     window::{PresentMode, WindowMode},
@@ -27,7 +26,8 @@ fn main() {
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
                 .with_system(shoulder_rotate)
-                .with_system(lower_arm_rotate),
+                .with_system(lower_arm_rotate)
+                .with_system(move_any_object),
         )
         // .add_plugin(DebugCursorPickingPlugin) // <- Adds the green debug cursor.
         // .add_plugin(DebugEventsPickingPlugin) // <- Adds debug event logging.
@@ -41,12 +41,6 @@ fn main() {
         })
         .run();
 }
-// Resource to hold the scene `instance_id` until it is loaded
-// #[derive(Default)]
-// struct SceneInstance(Option<InstanceId>);
-// // Component that will be used to tag entities in the scene
-// #[derive(Component)]
-// struct EntityInMyScene;
 
 #[derive(Component, Debug)]
 struct ShoulderRotate {
@@ -56,6 +50,11 @@ struct ShoulderRotate {
 #[derive(Component, Debug)]
 struct LowerArmRotate {
     rotation_speed: f32,
+}
+
+#[derive(Component, Debug)]
+struct MoveObject {
+    move_speed: f32,
 }
 fn setup(
     mut commands: Commands,
@@ -75,8 +74,9 @@ fn setup(
         .insert_bundle(PickingCameraBundle::default());
 
     // Spawn objects
-    let radian: f32 = PI / 180.;
     let rotation_speed: f32 = f32::to_radians(1.);
+    let move_speed: f32 = 0.001;
+
     commands
         .spawn_bundle(PbrBundle {
             mesh: asset_server.load("models/Gleb_Robot/base.obj"),
@@ -120,8 +120,8 @@ fn setup(
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
             transform: Transform::from_matrix(Mat4::from_scale_rotation_translation(
                 Vec3::new(1., 1., 1.),
-                Quat::from_rotation_y(180. * radian),
-                Vec3::new(0.0, 0.73, -1.47),
+                Quat::from_rotation_y(0.),
+                Vec3::new(0.0, 1.55, -1.25),
             )),
             ..Default::default()
         })
@@ -130,7 +130,12 @@ fn setup(
         .spawn_bundle(PbrBundle {
             mesh: asset_server.load("models/Gleb_Robot/upper_arm.obj"),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 0.9, 1.85),
+            // transform: Transform::from_xyz(0.0, 0.9, 1.85),
+            transform: Transform::from_matrix(Mat4::from_scale_rotation_translation(
+                Vec3::new(1., 1., 1.),
+                Quat::from_rotation_x(0.),
+                Vec3::new(0.0, 2.446, 0.48),
+            )),
             ..Default::default()
         })
         .insert_bundle(PickableBundle::default());
@@ -138,10 +143,16 @@ fn setup(
         .spawn_bundle(PbrBundle {
             mesh: asset_server.load("models/Gleb_Robot/wrist.obj"),
             material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-            transform: Transform::from_xyz(0.0, 1., 0.32),
+            // transform: Transform::from_xyz(0.0, 1., 0.32),
+            transform: Transform::from_matrix(Mat4::from_scale_rotation_translation(
+                Vec3::new(1., 1., 1.),
+                Quat::from_rotation_x(0.25844246),
+                Vec3::new(0.0, 2.368001, 0.79499954),
+            )),
             ..Default::default()
         })
-        .insert_bundle(PickableBundle::default());
+        .insert_bundle(PickableBundle::default())
+        .insert(MoveObject { move_speed });
 
     commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
@@ -159,7 +170,7 @@ fn setup(
         transform: Transform::from_xyz(5.0, 8.0, 5.0),
         ..default()
     });
-
+    // Blue background
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Capsule {
             // We make the dimensions negative because we want to invert the direction
@@ -198,15 +209,17 @@ fn shoulder_rotate(
 
 fn lower_arm_rotate(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&LowerArmRotate, &mut Transform)>,
+    mut query: Query<(&LowerArmRotate, &mut Transform), Without<ShoulderRotate>>,
+    mut query_2: Query<(&ShoulderRotate, &Transform)>,
 ) {
     let (object, mut transform) = query.single_mut();
+    let (shoulder, transform_shoulder) = query_2.single_mut();
+    // dbg!(shoulder);
     let mut rotation_factor = 0.0;
     if keyboard_input.pressed(KeyCode::Left) {
         if transform.rotation.x < 0.7 {
             rotation_factor += object.rotation_speed;
         }
-        dbg!(&transform.rotation.x);
     }
     if keyboard_input.pressed(KeyCode::Right) {
         if transform.rotation.x > -0.3 {
@@ -215,4 +228,37 @@ fn lower_arm_rotate(
     }
 
     transform.rotate(Quat::from_rotation_x(rotation_factor));
+}
+
+// Debug function
+// add Component MoveObject to debug
+// allows move and rotate object on one plane
+// Usage: "I" "K" "J" "L" "U" "O"
+fn move_any_object(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&MoveObject, &mut Transform)>,
+) {
+    let (obj, mut transform) = query.single_mut();
+    let movement_factor = obj.move_speed;
+    let rotation_speed: f32 = f32::to_radians(1.);
+    if keyboard_input.pressed(KeyCode::I) {
+        transform.translation.y += movement_factor;
+    }
+    if keyboard_input.pressed(KeyCode::K) {
+        transform.translation.y -= movement_factor;
+    }
+    if keyboard_input.pressed(KeyCode::J) {
+        transform.translation.z -= movement_factor;
+    }
+    if keyboard_input.pressed(KeyCode::L) {
+        transform.translation.z += movement_factor;
+    }
+    if keyboard_input.pressed(KeyCode::U) {
+        transform.rotate(Quat::from_rotation_x(rotation_speed));
+    }
+    if keyboard_input.pressed(KeyCode::O) {
+        transform.rotate(Quat::from_rotation_x(-rotation_speed));
+    }
+    dbg!(transform.translation.xyz());
+    dbg!(transform.rotation.x);
 }
