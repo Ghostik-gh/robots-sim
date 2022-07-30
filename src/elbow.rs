5f483733ba63fed307d4a843c94591e460185267
Ghostik-gh <feodor200@mail.ru>
2022-07-30 11:31:35 +0300

    fix movement

--- src/elbow.rs
+++ src/elbow.rs
@@ -12,12 +12,17 @@ pub fn elbow_rotate(
     let (object, mut transform) = query.single_mut();
     let mut rotation_factor = 0.0;
     if object.can_move {
-        if keyboard_input.pressed(KeyCode::Left) {
-            rotation_factor += object.rotation_speed;
+        if transform.rotation.x < 0.1 {
+            if keyboard_input.pressed(KeyCode::Left) {
+                rotation_factor += object.rotation_speed;
+                transform.rotate(Quat::from_rotation_x(rotation_factor));
+            }
         }
-        if keyboard_input.pressed(KeyCode::Right) {
-            rotation_factor -= object.rotation_speed;
+        if transform.rotation.x > -0.8 {
+            if keyboard_input.pressed(KeyCode::Right) {
+                rotation_factor -= object.rotation_speed;
+                transform.rotate(Quat::from_rotation_x(rotation_factor));
+            }
         }
-        transform.rotate(Quat::from_rotation_x(rotation_factor));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bevy::{
     core::FixedTimestep,
-    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     prelude::*,
     window::{PresentMode, WindowMode},
 };
@@ -10,6 +9,7 @@ use bevy_obj::*;
 use robots_sim::{InfiniteGridBundle, InfiniteGridPlugin};
 
 use robots_sim::elbow::*;
+use robots_sim::infotext_system::*;
 use robots_sim::lower_arm::*;
 use robots_sim::shoulder::*;
 use robots_sim::upper_arm::*;
@@ -27,6 +27,7 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(infotext_system)
+        .add_system(fps_system)
         .add_system(change_text_system)
         .add_startup_system(setup)
         .add_plugins(DefaultPickingPlugins)
@@ -58,189 +59,6 @@ fn main() {
 #[allow(unused)]
 struct MoveObject {
     move_speed: f32,
-}
-#[derive(Component)]
-struct TextChanges;
-fn infotext_system(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    commands.spawn_bundle(UiCameraBundle::default());
-    commands.spawn_bundle(TextBundle {
-        style: Style {
-            align_self: AlignSelf::FlexEnd,
-            position_type: PositionType::Absolute,
-            position: Rect {
-                top: Val::Px(5.0),
-                left: Val::Px(15.0),
-                ..default()
-            },
-            ..default()
-        },
-        text: Text::with_section(
-            "This is\ntext with\nline breaks\nin the top left",
-            TextStyle {
-                font: font.clone(),
-                font_size: 50.0,
-                color: Color::WHITE,
-            },
-            Default::default(),
-        ),
-        ..default()
-    });
-    commands.spawn_bundle(TextBundle {
-    style: Style {
-        align_self: AlignSelf::FlexEnd,
-        position_type: PositionType::Absolute,
-        position: Rect {
-            top: Val::Px(5.0),
-            right: Val::Px(15.0),
-            ..default()
-        },
-        max_size: Size {
-            width: Val::Px(400.),
-            height: Val::Undefined,
-        },
-        ..default()
-    },
-    text: Text::with_section(
-                "This text is very long, has a limited width, is centred, is positioned in the top right and is also coloured pink.",
-                    TextStyle {
-                font: font.clone(),
-                font_size: 50.0,
-                color: Color::rgb(0.8, 0.2, 0.7),
-            },
-        TextAlignment {
-            horizontal: HorizontalAlign::Center,
-            vertical: VerticalAlign::Center,
-        },
-    ),
-    ..default()
-});
-    commands
-        .spawn_bundle(TextBundle {
-            style: Style {
-                align_self: AlignSelf::FlexEnd,
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    bottom: Val::Px(5.0),
-                    right: Val::Px(15.0),
-                    ..default()
-                },
-                ..default()
-            },
-            text: Text {
-                sections: vec![
-                    TextSection {
-                        value: "This text changes in the bottom right".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::WHITE,
-                        },
-                    },
-                    TextSection {
-                        value: "\nThis text changes in the bottom right - ".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::RED,
-                        },
-                    },
-                    TextSection {
-                        value: "".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::ORANGE_RED,
-                        },
-                    },
-                    TextSection {
-                        value: " fps, ".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::YELLOW,
-                        },
-                    },
-                    TextSection {
-                        value: "".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::GREEN,
-                        },
-                    },
-                    TextSection {
-                        value: " ms/frame".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::BLUE,
-                        },
-                    },
-                ],
-                alignment: Default::default(),
-            },
-            ..default()
-        })
-        .insert(TextChanges);
-    commands.spawn_bundle(TextBundle {
-        style: Style {
-            align_self: AlignSelf::FlexEnd,
-            position_type: PositionType::Absolute,
-            position: Rect {
-                bottom: Val::Px(5.0),
-                left: Val::Px(15.0),
-                ..default()
-            },
-            size: Size {
-                width: Val::Px(200.0),
-                ..default()
-            },
-            ..default()
-        },
-        text: Text::with_section(
-            "This\ntext has\nline breaks and also a set width in the bottom left".to_string(),
-            TextStyle {
-                font,
-                font_size: 50.0,
-                color: Color::WHITE,
-            },
-            Default::default(),
-        ),
-        ..default()
-    });
-}
-fn change_text_system(
-    time: Res<Time>,
-    diagnostics: Res<Diagnostics>,
-    mut query: Query<&mut Text, With<TextChanges>>,
-) {
-    for mut text in query.iter_mut() {
-        let mut fps = 0.0;
-        if let Some(fps_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
-            if let Some(fps_avg) = fps_diagnostic.average() {
-                fps = fps_avg;
-            }
-        }
-
-        let mut frame_time = time.delta_seconds_f64();
-        if let Some(frame_time_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
-        {
-            if let Some(frame_time_avg) = frame_time_diagnostic.average() {
-                frame_time = frame_time_avg;
-            }
-        }
-
-        text.sections[0].value = format!(
-            "This text changes in the bottom right - {:.1} fps, {:.3} ms/frame",
-            fps,
-            frame_time * 1000.0,
-        );
-
-        text.sections[2].value = format!("{:.1}", fps);
-
-        text.sections[4].value = format!("{:.3}", frame_time * 1000.0);
-    }
 }
 fn setup(
     mut commands: Commands,
@@ -417,20 +235,35 @@ fn choise_object(
     let mut object3 = query3.single_mut();
     let mut object4 = query4.single_mut();
     let mut object5 = query5.single_mut();
-    object1.can_move = false;
-    object2.can_move = false;
-    object3.can_move = false;
-    object4.can_move = false;
-    object5.can_move = false;
     if keyboard_input.pressed(KeyCode::Key1) {
         object1.can_move = true;
+        object2.can_move = false;
+        object3.can_move = false;
+        object4.can_move = false;
+        object5.can_move = false;
     } else if keyboard_input.pressed(KeyCode::Key2) {
+        object1.can_move = false;
         object2.can_move = true;
+        object3.can_move = false;
+        object4.can_move = false;
+        object5.can_move = false;
     } else if keyboard_input.pressed(KeyCode::Key3) {
+        object1.can_move = false;
+        object2.can_move = false;
         object3.can_move = true;
+        object4.can_move = false;
+        object5.can_move = false;
     } else if keyboard_input.pressed(KeyCode::Key4) {
+        object1.can_move = false;
+        object2.can_move = false;
+        object3.can_move = false;
         object4.can_move = true;
+        object5.can_move = false;
     } else if keyboard_input.pressed(KeyCode::Key5) {
+        object1.can_move = false;
+        object2.can_move = false;
+        object3.can_move = false;
+        object4.can_move = false;
         object5.can_move = true;
     }
 }
